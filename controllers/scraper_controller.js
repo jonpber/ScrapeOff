6c3099f2a13f83a60b49e4a3ac48ebfae53571e0
Jon Berry <jonpber@gmail.com>
2017-09-07 14:20:28 -0700

    site all but functionally working. Saved article section needs work on modals. No mongoose implemented yet. Css still needs polish

--- controllers/scraper_controller.js
+++ controllers/scraper_controller.js
@@ -6,6 +6,8 @@ var request = require("request");
 var cheerio = require("cheerio");
 var mongojs = require("mongojs");
 
+var body = require("body-parser");
+
 // Database configuration
 var databaseUrl = "scraperWeb";
 var collections = ["scrapedData"];
@@ -22,7 +24,6 @@ router.get("/", function(req, res){
 	    }
 	    // Otherwise, send the result of this query to the browser
 	    else {
-	      console.log(found);
 			res.render("index", {articles: found});
 			// res.render("index", {});
 	    }
@@ -31,56 +32,105 @@ router.get("/", function(req, res){
 
 router.get("/scrape", function(req, res){
 	request("https://www.nytimes.com/section/world", function(error, response, html) {
-
-		var $ = cheerio.load(html);
-
-		var results = [];
+		let $ = cheerio.load(html);
+		let results = [];
 
 		$(".headline").each(function(i, element) {
 			if (i < 10){
-				var link = $(element).children("a").attr("href");
-				var title = $(element).text();
+				let link = $(element).children("a").attr("href");
+				let title = $(element).text();
+				console.log(title);
 
-				db.scrapedData.findOne({title: title}, function(error, found){
-					if (error){
-						throw error;
-					}
+				request(link, function(error1, response1, html1){
+					$1 = cheerio.load(html1);
 
-					if (found === null){
-						request(link, function(error1, response1, html1) {
-							// console.log("scraping for " + link);
-							$1 = cheerio.load(html1);
+					$1(".story-body-1").each(function(i1, element1){
+						if (i1 === 0){
+							let image = $1(element1).children(".media.photo").attr("itemid");
 
-							$1("div.image").each(function(i1, element1){
-								// console.log(i1)
-								if (i1 === 0){
-									var img = $1(element1).children("img").attr("src");
-									var summary = $1(element1).parent().parent().children("p.story-body-text").text();
-									summary = summary.slice(0, 250);
-									summary = summary + "... (click to read article)";
+							if (image == null){
+								image = "https://f4.bcbits.com/img/a1322149552_10.jpg";
+							}
+							// story-body-text
+							let summary = $1(element1).children(".story-body-text").text();
 
-									db.scrapedData.insert({
-										title: title,
-										link: link,
-										img: img,
-										saved: false,
-										summary: summary
-									});
-								}
-							})
-							db.scrapedData.find().sort({"_id": -1}, function(error, found){
-								res.json(found);
-							})
+							summary = summary.slice(0, 250);
+							summary = summary + "... (click to read article)";
 
-						})
-					}
+							db.scrapedData.update({title: title}, {
+								title: title,
+								link: link,
+								img: image,
+								summary: summary,
+								saved: false
+							}, {upsert: true}, function(error, found){
+							});
+						}
+					})
 				})
 
 				
-				
 
-				
 			}
+
+			else if (i === 11) {
+				db.scrapedData.find({saved: false}).sort({"_id": -1}, function(error, found){
+				  	if (error) {
+				      console.log(error);
+				    }
+				    // Otherwise, send the result of this query to the browser
+				    else {
+						res.json(found);
+				    }
+				  })
+			}
+
+
+
+
+				// db.scrapedData.findOne({title: title}, function(error, found){
+				// 	if (error){
+				// 		throw error;
+				// 	}
+
+				// 	if (found === null){
+				// 		request(link, function(error1, response1, html1) {
+				// 			// console.log("scraping for " + link);
+				// 			$1 = cheerio.load(html1);
+
+				// 			$1("div.image").each(function(i1, element1){
+				// 				// console.log(i1)
+				// 				if (i1 === 0){
+				// 					var img = $1(element1).children("img").attr("src");
+				// 					var summary = $1(element1).parent().parent().children("p.story-body-text").text();
+				// 					summary = summary.slice(0, 250);
+				// 					summary = summary + "... (click to read article)";
+
+				// 					db.scrapedData.insert({
+				// 						title: title,
+				// 						link: link,
+				// 						img: img,
+				// 						saved: false,
+				// 						summary: summary,
+				// 						comments: []
+				// 					});
+				// 				}
+				// 			})
+							
+				// 		})
+				// 	}
+				// })		
+
+				// db.scrapedData.find({saved: false}).sort({"_id": -1}, function(error, found){
+				//   	if (error) {
+				//       console.log(error);
+				//     }
+				//     // Otherwise, send the result of this query to the browser
+				//     else {
+				// 		res.send(found);
+				//     }
+				//   })
+			
 		});
 	})
 });
@@ -100,10 +150,26 @@ router.get("/saved", function(req, res){
 	})
 });
 
+router.get("/articles/:id", function(req, res){
+	db.scrapedData.findOne({"_id": ObjectId(req.params.id)}, function(error, found){
+		res.json(found);
+	})
+})
+
 router.put("/articles/:id", function(req, res){
-	console.log(req.params.id);
 	db.scrapedData.update({"_id": ObjectId(req.params.id)}, {$set: {saved: true}}, function(error, found){
 		res.redirect("/")
+	})
+})
+
+router.post("/articles/:id", function(req, res){
+	db.scrapedData.update({"_id": ObjectId(req.params.id)}, {$addToSet: {comments: req.body.comment}}, function(error, found){
+	})
+})
+
+router.delete("/articles/:id", function(req, res){
+	db.scrapedData.remove({"_id": ObjectId(req.params.id)}, function(error, found){
+		res.redirect("/saved")
 	})
 })
 
--- public/assets/javascript/site.js
+++ public/assets/javascript/site.js
@@ -1,53 +1,92 @@
 $(function(){
-	$(".scrapeBtn").on("click", function(){
-		$.getJSON("/scrape", function(data) {
-			// updateScrapedArticles(data);
-		});
+	$('.readIcon').webuiPopover({content:'Read Article', animation:'pop', placement:"right", trigger:'hover', delay: {
+		show: 750,
+		hide: 100
+	}});
+
+	$('.saveIcon').webuiPopover({content:'Save Article', animation:'pop', placement:"right", trigger:'hover', delay: {
+		show: 750,
+		hide: 100
+	}});
+
+	$('.deleteArticleIcon').webuiPopover({content:'Delete Article', animation:'pop', placement:"right", trigger:'hover', delay: {
+		show: 750,
+		hide: 100
+	}});
+
+	$('.commentIcon').webuiPopover({content:'Comment on Article', animation:'pop', placement:"right", trigger:'hover', delay: {
+		show: 750,
+		hide: 100
+	}});
+
+	$("body").on("click", ".commentIcon", function(){
+		let id = $(this).attr("dataId");
+		$.get("/articles/" + id, function(data, status){
+			let modal = $("<div class='modal'>");
+			let modalContent = $("<div class='content'>");
+			modalContent.append("<h3>Comments</h3>");
+			let commentDiv = $("<div class='commentDiv'>")
+			if (data.comments !== undefined){
+				for (let i = 0; i < data.comments.length; i++){
+					let comment = $("<div class='comment valign-wrapper'>")
+					.append("<p class='commentText'>" + data.comments[i] + "</p>")
+					.append("<button class='removeBtn'>X</button>")
+					.appendTo(commentDiv);
+				}
+			}
+
+			commentDiv.appendTo(modalContent);
+			modalContent.append("<hr>")
+			.append("<h3>Add Comment</h3>")
+			let submitForm = $("<form action='/articles/"+ id +"' method='POST'>")
+			.append("<textarea class='newComment' name='comment'>")
+			.append("<button type='submit' class='submitComment'>Submit</button>")
+			.appendTo(modalContent);
+			modalContent.appendTo(modal);
+			$(modal).iziModal();
+			$("body").append(modal);
+			$(modal).iziModal('open');
+		})
 	})
 
-	// function updateScrapedArticles(data){
-	// 	$(".mainPageRow").empty();
+	$("body").on("click", ".scrapeBtn", function(event){
+		event.preventDefault();
+		$.get("/scrape", function(data, status){
+			console.log(data);
+			$(".mainPageRow").empty();
+			for (let i = 0; i < data.length; i++){
+				if (i === 0){
+					let column = $("<div class='col s12 center'>")
+					let article = $("<div class='article'>")
+					.append("<a href='" + data[i].link +"'class='headline'><h1 class='topH1'>" + data[i].title +"</h1></a>")
+					.append("<img src='" + data[i].img + "' class='topImg'>")
+					.append("<p>" + data[i].summary + "</p>")
+					let saveForm = $("<form action='/articles/" + data[i].title_id + "?_method=PUT' method='POST'>")
+					.append("<button type='submit' class='saveArticle'>Save Article  <span class='material-icons saveIcon'>fiber_pin</span></button>")
+					.appendTo(article);
+					article.append("<hr>")
+					column.append(article).appendTo(".mainPageRow");
+				}
+				
+				else {
+					let column = $("<div class='col s12 center'>")
+					let article = $("<div class='article smallArticle'>")
+					let articleHeadlineCol = $("<div class='col s11'>")
+					.append("<a href='" + data[i].link + "' class='headline'><h3>" + data[i].title + "</h3></a>")
+					.appendTo(article);
+					let buttonCol = $("<div class='col s1 iconButton'>")
+					let saveForm = $("<form action='/articles/" + data[i].title_id + "?_method=PUT' method='POST'>")
+					.append("<button type='submit' class='saveArticle material-icons saveIcon'>fiber_pin</button>")
+					.appendTo(article);
+					column.append(article).appendTo(".mainPageRow");
 
-	// 	for (var i = 0; i < data.length; i++){
-	// 		if (i === 0){
-	// 			var topDiv = $("<div class='col s12'>");
-	// 			var articleDiv = $("<div class='article'>");
-	// 			articleDiv.append("<h1 class='topH1'>" + data[i].title  + "</h1>")
-	// 			.append("<img src=" + data[i].img + " class='topImg'>")
-	// 			.append("<p>" + data[i].summary + "</p>")
-	// 			topDiv.append(articleDiv).appendTo(".mainPageRow")
-	// 			$(".mainPageRow").append("<hr>")
-	// 		}
 
-	// 		else if (i > 0 && i < 4){
-	// 			var midDiv = $("<div class='col s4'>");
-	// 			var articleDiv = $("<div class='article smallArticle'>");
-	// 			articleDiv.append("<h3>" + data[i].title  + "</h3>")
-	// 			.append("<img src=" + data[i].img + " class='smallImg'>")
-	// 			midDiv.append(articleDiv).appendTo(".mainPageRow")
+				}
+			}
+		})
+	})
 
-	// 			if (i === 3){
-	// 				$(".mainPageRow").append("<hr>")
-	// 			}
-	// 		}
 
-	// 		else {
-	// 			var headlinesDiv = $("<div class='col s4'>");
-	// 			var articleDiv = $("<div class='article smallArticle'>");
-	// 			articleDiv.append("<h3 class='smallerHeadline'>" + data[i].title  + "</h3>")
-	// 			headlinesDiv.append(articleDiv).appendTo(".mainPageRow")
-	// 		}
-	// 	}
-
-		$('.readIcon').webuiPopover({content:'Read Article', animation:'pop', placement:"right", trigger:'hover', delay: {
-			show: 750,
-			hide: 100
-		}});
-
-		$('.saveIcon').webuiPopover({content:'Save Article', animation:'pop', placement:"right", trigger:'hover', delay: {
-			show: 750,
-			hide: 100
-		}});
 	// }
 
 	// $.getJSON("/scrape", function(data) {
